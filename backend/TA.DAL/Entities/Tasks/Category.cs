0981e444b42e16ec5b1fcc2d9bd37f667f8b02d8
Am0rr <itsaizel468@gmail.com>
2026-06-24 17:32:01 +0300

    feat(DAL): add Category entity

---
+++ backend/TA.DAL/Entities/Tasks/Category.cs
@@ -0,0 +1,20 @@
+namespace TA.DAL.Entities.Tasks;
+
+public class Category : BaseEntity
+{
+    public string Name { get; private set; } = null!;
+    public string? Description { get; private set; }
+    public Guid UserId { get; private set; }
+
+    protected Category() { }
+
+    public Category(string name, string description, Guid userId)
+    {
+        Name = name;
+        Description = description;
+        UserId = userId;
+    }
+
+    public void ChangeName(string name) => Name = name;
+    public void ChangeDescription(string description) => Description = description;
+}
