2cb24b8b792df2c5cc8375b49c3c8ed7c6954383
Am0rr <itsaizel468@gmail.com>
2026-06-24 17:46:20 +0300

    feat(DAL): add AppDbContext

---
+++ backend/TA.DAL/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using TA.DAL.Entities.Identity;
+using TA.DAL.Entities.Tasks;
+
+namespace TA.DAL.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<TaskItem> TaskItems { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
