c465b4d97b87fafb51ef8ba5f732b7914811fdba
Am0rr <itsaizel468@gmail.com>
2026-06-25 13:52:40 +0300

    feat(DAL): create ITaskRepository interface

---
+++ backend/TA.DAL/Interfaces/ITaskRepository.cs
@@ -0,0 +1,9 @@
+using TA.DAL.Entities.Tasks;
+using TA.DAL.Models;
+
+namespace TA.DAL.Interfaces;
+
+public interface ITaskRepository : IBaseRepository<TaskItem>
+{
+    Task<TaskPagedResult> GetFilteredPagedAsync(Guid userId, TaskFilterModel filter, CancellationToken cancellationToken = default);
+}
