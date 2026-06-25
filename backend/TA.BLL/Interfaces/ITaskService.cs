3255223cca66f56f094a5edfaf2e734a2df7c29b
Am0rr <itsaizel468@gmail.com>
2026-06-25 16:41:38 +0300

    feat(BLL): create ITaskService interface

--- backend/TA.BLL/Interfaces/ITaskService.cs
+++ backend/TA.BLL/Interfaces/ITaskService.cs
@@ -1,13 +1,14 @@
 using TA.BLL.DTOs.Tasks;
+using TA.DAL.Models;
 
 namespace TA.BLL.Interfaces;
 
 public interface ITaskService
 {
-    Task<TaskResponse> CreateAsync(Guid id, Guid userId, CreateTaskRequest request, CancellationToken cancellationToken = default);
-    Task UpdateAsync(Guid id, UpdateTaskRequest request, CancellationToken cancellationToken = default);
-    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<TaskResponse> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<IEnumerable<TaskResponse>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<TaskPagedResponse>
+    Task<TaskResponse> CreateAsync(Guid userId, CreateTaskRequest request, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Guid id, Guid userId, UpdateTaskRequest request, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
+    Task<TaskResponse> GetByIdAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TaskResponse>> GetAllAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<TaskPagedResponse> GetPagedAsync(Guid userId, TaskFilterModel filter, CancellationToken cancellationToken = default);
 }
