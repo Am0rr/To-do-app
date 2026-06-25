22008bab9615e66e21ebda7d5cb7c6902743a26a
Am0rr <itsaizel468@gmail.com>
2026-06-25 16:12:22 +0300

    feat(BLL): implement update and create Category request validators

---
+++ backend/TA.BLL/Validators/Tasks/UpdateCategoryValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using TA.BLL.DTOs.Tasks;
+
+namespace TA.BLL.Validators.Category;
+
+public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryRequest>
+{
+    public UpdateCategoryValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Category name cannot be empty.")
+            .MaximumLength(100).WithMessage("Category name must not exceed 100 characters.")
+            .When(c => c.Name != null);
+
+        RuleFor(c => c.Description)
+            .NotEmpty().WithMessage("Category description cannot be empty.")
+            .MaximumLength(1000).WithMessage("Category description must not exceed 1000 characters.")
+            .When(c => c.Description != null);
+
+        RuleFor(c => c.UserId)
+            .NotEmpty().WithMessage("User ID is required")
+            .When(c => c.UserId != null);
+    }
+}
