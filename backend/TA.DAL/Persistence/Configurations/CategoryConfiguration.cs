6f237c653d3d9187889ee46bcf1bd3dbd8232568
Am0rr <itsaizel468@gmail.com>
2026-06-25 12:40:06 +0300

    feat(DAL): add CategoryConfiguration

---
+++ backend/TA.DAL/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TA.DAL.Entities.Identity;
+using TA.DAL.Entities.Tasks;
+
+namespace TA.DAL.Persistence.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.CreatedAt).IsRequired();
+        builder.Property(c => c.Name).IsRequired().HasMaxLength(100);
+        builder.Property(c => c.Description).IsRequired(false).HasMaxLength(500);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(c => c.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
