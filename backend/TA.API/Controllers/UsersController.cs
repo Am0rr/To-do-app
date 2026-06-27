31646ef2c5d6bbcd7b9dde7903a18a43ce937cea
Am0rr <itsaizel468@gmail.com>
2026-06-27 12:27:57 +0300

    refactor(API): make UsersController use primary constructor

--- backend/TA.API/Controllers/UsersController.cs
+++ backend/TA.API/Controllers/UsersController.cs
@@ -9,47 +9,40 @@ namespace TA.API.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 [Authorize(Roles = nameof(UserRole.Administrator))]
-public class UsersController : ControllerBase
+public class UsersController(IUserService userService) : ControllerBase
 {
-    private readonly IUserService _userService;
-
-    public UsersController(IUserService userService)
-    {
-        _userService = userService;
-    }
-
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<UserResponse>> GetById(Guid id, CancellationToken cancellationToken)
     {
-        var user = await _userService.GetByIdAsync(id, cancellationToken);
+        var user = await userService.GetByIdAsync(id, cancellationToken);
         return Ok(user);
     }
 
     [HttpGet("email")]
     public async Task<ActionResult<UserResponse>> GetByEmail([FromQuery] string email, CancellationToken cancellationToken)
     {
-        var user = await _userService.GetByEmailAsync(email, cancellationToken);
+        var user = await userService.GetByEmailAsync(email, cancellationToken);
         return Ok(user);
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<UserResponse>>> GetAll(CancellationToken cancellationToken)
     {
-        var users = await _userService.GetAllAsync(cancellationToken);
+        var users = await userService.GetAllAsync(cancellationToken);
         return Ok(users);
     }
 
     [HttpPatch("{id:guid}")]
     public async Task<IActionResult> Update(Guid id, [FromBody] UpdateUserRequest request, CancellationToken cancellationToken)
     {
-        await _userService.UpdateAsync(id, request, cancellationToken);
+        await userService.UpdateAsync(id, request, cancellationToken);
         return NoContent();
     }
 
     [HttpDelete("{id:guid}")]
     public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
     {
-        await _userService.DeleteAsync(id, cancellationToken);
+        await userService.DeleteAsync(id, cancellationToken);
         return NoContent();
     }
 }
